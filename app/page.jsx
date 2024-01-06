b36229f19e69286f99abed766e49e4e658b00505
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-01-06 15:42:25 +0900

    게시글 페이지 추가

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,11 @@
+"use client"
+
+import { useEffect } from "react";
+
 export default function app() {
+    useEffect(() => {
+        console.log("렌더링");
+    }, []);
     return (
         <>
             <svg width={500} height={500}></svg>
