bba7d5300de980ca1f5b9a184d743d29a0e097f8
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-02-01 17:34:10 +0900

    그래프 데이터 처리 코드 수정 - svg의 title 관련 코드 제거

--- app/util/getGraphStructure.jsx
+++ app/util/getGraphStructure.jsx
@@ -1,22 +1,17 @@
-// import { fetchData } from "../util/fetchGraphData.js";
+// import { fetchdata } from "../util/fetchGraphdata.js";
 import data from "../util/nodeList.json";
 const nodes = [];
 const links = [];
-let id = 0;
+
 export async function getGraphStructure() {
-    // const data = await fetchData("graph_view");
-    for (const el of data) {
-        processData(el);
+    // const data = await fetchdata("graph_view");
+    for (const node of data) {
+        nodes.push({ id: node.id, label: node.label });
+        if (node.target_array && node.target_array.length !== 0) {
+            node.target_array.forEach((el) => {
+                links.push({ source: node.id, target: el });
+            });
+        }
     }
     return { nodes, links };
 }
-
-const processData = (data) => {
-    nodes.push({ id: id, label: data.label, title: data.title });
-    id++;
-    if (data.target_array) {
-        data.target_array.forEach((el) => {
-            links.push({ source: data.label, target: el });
-        });
-    }
-};
