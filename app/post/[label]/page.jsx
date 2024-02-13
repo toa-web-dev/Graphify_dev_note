7aee70d1ce78946f1efcf92d050926950f04a9a9
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-02-13 16:05:13 +0900

    ✨게시글 페이지 기능 추가 - 인코딩 모듈로 url 처리 코드 추가 - 뒤로가기 버튼 추가 - 작성완료가 아닌 게시글의 경우 미완성 문구를 띄움

--- app/post/[label]/page.jsx
+++ app/post/[label]/page.jsx
@@ -4,28 +4,34 @@ import styles from "@/app/style/Post.module.scss";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import MarkdownRenderer from "@/app/components/MarkdownRenderer.jsx";
+import { underscoreToSpace } from "@/app/util/replaceEncodeUrl";
+import Link from "next/link";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-const anonKey = process.env.NEXT_PUBLIC_API_KEY;
-
+const query = `?label=eq.`;
 export default function Article() {
     const param = useParams();
     const [postContent, setPostContent] = useState(null);
     useEffect(() => {
-        const path = param.label;
+        const path = underscoreToSpace(param.label);
+        const queryString = query + path;
         (async () => {
-            const data = await fetchData("post", `?select=content&label_fk=eq.${param.label}`);
+            const data = await fetchData("post", queryString);
             setPostContent(...data);
         })();
     }, []);
 
     return (
         <>
-            <main className={styles.page}>
-                <article>
-                    {postContent && <MarkdownRenderer content={postContent.content} />}
-                </article>
-            </main>
+            {postContent && (
+                <main className={styles.page}>
+                    <Link href="/">←</Link>
+                    {postContent.is_completed ? null : <div>아직 작성중인 글입니다.</div>}
+                    <article>
+                        <div>{postContent.createdAt}</div>
+                        <MarkdownRenderer content={postContent.content} />
+                    </article>
+                </main>
+            )}
         </>
     );
 }
