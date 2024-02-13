b06201b88c6acfc88c329c148c37154e63072bb7
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-02-13 15:55:26 +0900

    ✨ 그래프 데이터에 게시글의 작성상태 속성을 추가

--- app/util/getGraphStructure.js
+++ app/util/getGraphStructure.js
@@ -2,15 +2,14 @@ import { fetchData } from "../util/fetchData.js";
 export async function getGraphStructure() {
     const nodes = [];
     const links = [];
-    const data = await fetchData("graph_data_view");
+    const data = await fetchData("graph_data");
     for (const item of data) {
-        nodes.push({ id: item.source, label: item.source });
+        nodes.push({ id: item.source, label: item.source, isCompleted: item.is_completed });
         if (item.targets && item.targets.length !== 0) {
             item.targets.forEach((el) => {
                 links.push({ source: item.source, target: el });
             });
         }
     }
-    console.log("nodes: ", nodes, "links: ", links);
     return { nodes, links };
 }
