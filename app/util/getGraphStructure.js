c66fda7466fc5f48531c1dfed8848e8a6706e26e
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-03-27 19:04:01 +0900

    nodes 데이터 추가

--- app/util/getGraphStructure.js
+++ app/util/getGraphStructure.js
@@ -4,7 +4,7 @@ export async function getGraphStructure() {
     const links = [];
     const data = await fetchData("graph_data");
     for (const item of data) {
-        nodes.push({ id: item.source, label: item.source, isCompleted: item.is_completed });
+        nodes.push({ id: item.source, label: item.source, ...item });
         if (item.targets && item.targets.length !== 0) {
             item.targets.forEach((el) => {
                 links.push({ source: item.source, target: el });
