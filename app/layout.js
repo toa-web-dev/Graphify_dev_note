b7b340b834abb96f7d8f09ab701a97e75db0995d
gimHyoungSoo <gimhyoungsoo@gmail.com>
2024-03-27 18:03:24 +0900

    metadata 변경

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,14 @@ import "./style/globals.css";
 // const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-    title: "Graphify_dev_note",
-    description: "지식을 도식화 하자!",
+    title: "개발 노트를 한눈에, Graphify_dev_note",
+    description:
+        "개발 학습을 위해 블로깅하는 문서를 네트워크 그래프로 시각화하여 개념을 정리하고 다른 내용과의 연관성을 쉽게 확인 할 수 있는 프로젝트입니다.",
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            {/* <body className={inter.className}>{children}</body> */}
             <body>{children}</body>
         </html>
     );
